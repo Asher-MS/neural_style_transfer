6b8c5bd7c4bdf0501a0fa69b86424f8f7a53a8cb
Asher Mathews <msasher123@gmail.com>
2023-05-21 22:45:33 +0530

    added semantic segmantation code

--- PhotoWCT_4_sonar/semantic_segmentation_test.py
+++ PhotoWCT_4_sonar/semantic_segmentation_test.py
@@ -1,8 +1,35 @@
-from rembg import remove
 from PIL import Image
+import requests
+from rembg import remove
 import sys
-input=Image.open(sys.argv[1])
+def paste_foreground_on_background(foreground_image, background_path, output_path):
+    
+    foreground_image = Image.open(foreground_image)
 
-output=remove(input)
+    
+    if foreground_image.mode != "RGBA":
+        foreground_image = foreground_image.convert("RGBA")
 
-output.save("output_image.png")
+    
+    with remove(foreground_image) as alpha_mask:
+      
+        background_image = Image.open(background_path)
+
+        
+        foreground_image = foreground_image.resize(background_image.size, Image.ANTIALIAS)
+        
+        if foreground_image.mode != background_image.mode:
+            foreground_image = foreground_image.convert(background_image.mode)
+        
+        alpha_mask=alpha_mask.resize(background_image.size,Image.ANTIALIAS)
+
+        background_image.paste(foreground_image, (0, 0),mask=alpha_mask)
+
+        
+        background_image.save(output_path)
+
+foreground_image = sys.argv[1]
+background_path = sys.argv[2]
+output_path = "output.jpg"
+
+paste_foreground_on_background(foreground_image, background_path, output_path)
